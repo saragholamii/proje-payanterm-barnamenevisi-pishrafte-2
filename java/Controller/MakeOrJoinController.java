53b53553c0e9e656b40aff40d18fe3fdd32e171e
saragholamii <ohartemis.2002@gmail.com>
2022-06-24 11:29:45 +0430

    neveshtan method onAction baraye IjadBaziJadidBtn

--- java/Controller/MakeOrJoinController.java
+++ java/Controller/MakeOrJoinController.java
@@ -1,14 +1,18 @@
 package Controller;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.Loader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import Model.*;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -39,7 +43,23 @@ public class MakeOrJoinController implements Initializable {
         this.clientFX = clientFX;
     }
 
-    public void ijadBaziJadidBtn(ActionEvent actionEvent) {
+    public void ijadBaziJadidBtn(ActionEvent actionEvent) throws IOException {
+        //***** hazf item haye ruye Scene.
+        ((Pane) goruhJadid.getParent()).getChildren().remove(goruhJadid);
+        ((Pane) lBHa.getParent()).getChildren().remove(lBHa);
 
+        //***** ijad loader jadid baraye load kardan safhe ijadBaziJadid.
+        FXMLLoader l = new FXMLLoader(getClass().getResource("/FXML/EntekhabMozuBarayeBaziJadid.fxml"));
+        Pane p = l.load();
+
+        //***** gereftan controller pane jadid baraye seda zadan method hayash.
+        EntekhabItemBarayeIjadBaziController c = (EntekhabItemBarayeIjadBaziController) l.getController();
+
+        //***** ferestadan ClientFX be in safhe.
+        c.setClientFX(clientFX);
+
+        //***** sakht Scene jadid va set kardan on ruye primary stage safhe asli.
+        Scene sc = new Scene(p);
+        clientFX.setSceneJadid(sc);
     }
 }
