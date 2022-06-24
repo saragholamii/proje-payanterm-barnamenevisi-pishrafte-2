8964390e8c6ec2c70755104b90a2314ab2e00196
saragholamii <ohartemis.2002@gmail.com>
2022-06-24 10:56:15 +0430

    controller safhe fxml entekhab item baraye bazi jadid

--- java/Controller/EntekhabItemBarayeIjadBaziController.java
+++ java/Controller/EntekhabItemBarayeIjadBaziController.java
@@ -1,2 +1,19 @@
-package Controller;public class EntekhabItemBarayeIjadBaziController {
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EntekhabItemBarayeIjadBaziController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void sakhtanSafheBazi(ActionEvent actionEvent) {
+    }
 }
